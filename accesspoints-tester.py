5c291ccb7bff4322db91d3e19f1572303ff75d63
Thibault Clerice <leponteineptique@gmail.com>
2014-01-23 16:50:13 +0000

    igraph in accesspoint tester

--- accesspoints-tester.py
+++ accesspoints-tester.py
@@ -11,4 +11,36 @@ from pprint import pprint
 
 ehri = EHRI()
 ehri.get(match = " MATCH (auth)<-[:relatesTo]-(description)-[:describes]->(doc)  ", fields = [("description.name", "title"), ("auth.name", "accessPoint")])
-print ehri.descriptions[0]
+
+#We create the index following these new authorities
+auth = Authorities()
+for item in ehri.descriptions:
+
+	if item["idDoc"] not in auth.index["items"]:
+		auth.index["items"][item["idDoc"]] = []
+
+	a = item["accessPoint"].split("-")
+
+	auth.index["items"][item["idDoc"]] += a
+	auth.index["authorities"] += a
+
+auth.index["authorities"] = list(set(auth.index["authorities"]))
+
+
+#Saving everything
+auth.save(nodesName = "ehri-accesspoints-nodes-all.csv", edgesName = "ehri-accesspoints-edges-all.csv")
+print "SAVING Normal DONE"
+
+auth.countAuthorities(purge=True)
+
+auth.stats()
+
+auth.thresholder(compute=128)
+
+auth.stats()
+
+auth.cluster()
+
+auth.save(nodesName = "ehri-accesspoints-nodes-cluster.csv", edgesName = "ehri-accesspoints-cluster.csv", mode="cluster")
+
+auth.iGraph(mode = "cluster", debug = True, output="ehri-accesspoints.graphml")
